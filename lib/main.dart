6f7a7a2babc3aaad7be0ac5c9b06f1afc3d76f92
Rishita <rishita@Rishitas-MacBook-Air.local>
2023-03-26 17:16:40 +0530

    Update Splash screen UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:my_guard/screens/home_screen.dart';
 import 'package:my_guard/screens/splash_screen.dart';
 
 import 'screens/login.dart';
@@ -20,40 +19,8 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       //home: const MyHomePage(),
-      home: const MyLogin(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key}) : super(key: key);
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('My Guard'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: null,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      //home: const MyLogin(),
+      home: const SplashScreen(),
     );
   }
 }
--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,10 +1,6 @@
-import 'dart:io';
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
-import 'package:my_guard/screens/home_screen.dart';
-import 'package:my_guard/screens/login.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
@@ -17,37 +13,29 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
-    Future.delayed(const Duration(seconds: 3), () {
-      Navigator.pushReplacement(
-          context, MaterialPageRoute(builder: (context) => const MyLogin()));
-    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.black,
-      body: Center(
-          child: Column(
+    return _buildSplashWidget();
+  }
+
+  Widget _buildSplashWidget() {
+    return Container(
+      width: double.maxFinite,
+      height: double.maxFinite,
+      color: Colors.black,
+      child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Image.asset(
-            'assets/image/womencircle.png',
-            height: 130,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(100.0),
+            child: Image.asset(
+              'assets/image/img_women_circle.png',
+            ),
           ),
-          const SizedBox(
-            height: 30,
-          ),
-          if (Platform.isIOS)
-            const CupertinoActivityIndicator(
-              radius: 15,
-            )
-          else
-            const CircularProgressIndicator(
-              color: Colors.black,
-            )
         ],
-      )),
+      ),
     );
   }
 }
