0c7c5171d1bebbd241fb6314b8ff824ca6d01e00
Rishita <rishita@Rishitas-MacBook-Air.local>
2023-03-26 21:09:01 +0530

    Modifications in splash screen

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,6 +1,9 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:my_guard/screens/home_screen.dart';
+import 'package:my_guard/screens/login.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
@@ -10,16 +13,30 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+  late bool _isLoginStateRetrieved;
+
+  User? _user;
+
+  //region: Overridden Functions
+
   @override
   void initState() {
     super.initState();
+    _initScreenVariables();
   }
 
   @override
   Widget build(BuildContext context) {
-    return _buildSplashWidget();
+    if (_isLoginStateRetrieved == false) {
+      return _buildSplashWidget();
+    }
+
+    return _user == null ? const MyLogin() : const HomeScreen();
   }
 
+//endregion
+
+  //region: Widgets
   Widget _buildSplashWidget() {
     return Container(
       width: double.maxFinite,
@@ -38,4 +55,21 @@ class _SplashScreenState extends State<SplashScreen> {
       ),
     );
   }
+
+  //endregion
+
+  //region: Private Functions
+
+  void _initScreenVariables() {
+    _isLoginStateRetrieved = false;
+    _user = FirebaseAuth.instance.currentUser;
+
+    Future<dynamic>.delayed(const Duration(seconds: 2), () {
+      setState(() {
+        _isLoginStateRetrieved = true;
+      });
+    });
+  }
+
+//endregion
 }
