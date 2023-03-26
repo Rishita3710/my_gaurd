5a0a57e57d500b20d921431465959f374860143c
Rishita <rishita@Rishitas-MacBook-Air.local>
2023-03-26 19:32:54 +0530

    Remove unused images

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:my_guard/screens/splash_screen.dart';
 
 import 'screens/login.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -18,8 +21,6 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      //home: const MyHomePage(),
-      //home: const MyLogin(),
       home: const SplashScreen(),
     );
   }
